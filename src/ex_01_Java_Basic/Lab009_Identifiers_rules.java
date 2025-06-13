805a00d855f9c05d5213bf241699b8aac5051ab2
anbup369 <anbu.vpm@gmail.com>
2025-06-13 23:07:53 +0530

    Java program with rules for identifiers and list of keywords

---
+++ src/ex_01_Java_Basic/Lab009_Identifiers_rules.java
@@ -0,0 +1,17 @@
+package ex_01_Java_Basic;
+
+public class Lab009_Identifiers_rules {
+    public static void main(String[] args) {
+        System.out.println(
+                """
+                Rules for Identifiers in Java":
+                - Can only contain letters (A–Z, a–z), digits (0–9), underscores (_) or dollar signs ($).
+                - Cannot start with a digit.
+                - No spaces or special characters like @, #, %, etc.
+                - Cannot be a Java reserved keyword, like class, if, else, static, etc.
+                - Case-sensitive — Name and name are different identifiers."""
+
+);
+    }
+}
+
---
+++ src/ex_01_Java_Basic/Lab010_Keywords_Identifiers.java
@@ -0,0 +1,35 @@
+package ex_01_Java_Basic;
+
+public class Lab010_Keywords_Identifiers {
+    public static void main(String[] args) {
+        System.out.println("How many keywords do you see there?, Identifier");
+        System.out.println("""
+    Total 53 keywords as of JDK 21.
+    In Java, keywords are reserved words that have a predefined meaning in the language.
+    They cannot be used as identifiers (like variable names, method names, or class names).
+    Here are some common Java keywords:
+    
+    - class – Defines a class.
+    - interface – Defines an interface.
+    - extends – Indicates inheritance between classes.
+    - implements – Shows that a class implements an interface.
+    - new – Used to create new objects.
+    - this – Refers to the current instance of a class.
+    - super – Refers to the parent class of the current object.
+    - static – Used to declare static members.
+    - final – Used to declare constants or prevent modification.
+    - public, private, protected – Access modifiers.
+    - if, else, switch, case – Control flow statements.
+    - for, while, do – Looping constructs.
+    - try, catch, finally, throw, throws – Exception handling.
+    - return – Used to exit from a method and return a value.
+    - break, continue – Used in loops and switch statements.
+    - volatile, synchronized – Related to multithreading.
+    - import, package – Used for organizing classes.
+    """);
+
+
+
+    }
+}
+
