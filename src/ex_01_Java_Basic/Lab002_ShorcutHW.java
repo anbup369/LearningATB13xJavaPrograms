fd61c32627096d71c12e45d53c2f517ef2af220b
anbup369 <anbu.vpm@gmail.com>
2025-06-13 22:04:48 +0530

    second java program for checking duplicate main function

---
+++ src/ex_01_Java_Basic/Lab002_ShorcutHW.java
@@ -0,0 +1,14 @@
+package ex_01_Java_Basic;
+
+public class Lab002_ShorcutHW {
+    public static void main(String[] args) {
+        System.out.println("Hello Test");
+    }
+
+//        public static void main(String[] args) { //this duplicate mentioned should be removed
+//
+//    }
+    public static void main(int args) {    //this duplicate main method is not entry point and it is not used
+        System.out.println("Hello Test");
+    }
+}
