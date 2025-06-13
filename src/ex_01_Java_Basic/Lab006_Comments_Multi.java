e5f928f2ff2b49a2914acab14e1663d71d1dd13c
anbup369 <anbu.vpm@gmail.com>
2025-06-13 22:31:32 +0530

    java program with comments and its usage examples

---
+++ src/ex_01_Java_Basic/Lab006_Comments_Multi.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+public class Lab006_Comments_Multi {
+    public static void main(String[] args) {
+        /* line1 :
+         * line2 :
+         * line3 :
+         * line4 :
+         */
+        System.out.println("Multi Comment");
+    }
+}
---
+++ src/ex_01_Java_Basic/Lab007_Comments_ShortCuts.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Lab007_Comments_ShortCuts {
+    // This program prints 'Hi' as output
+    // For QA: this will print the hi in console
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
