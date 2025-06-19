3136faa0805d9a6e229d130e7d420f0298e893e3
anbup369 <anbu.vpm@gmail.com>
2025-06-19 20:05:03 +0530

    Coding Challenge_2 : Arithmetic and Assignment Operators

---
+++ src/ex_Java_Test_19th_June_2025/Coding_challenge_2.java
@@ -0,0 +1,27 @@
+package ex_Java_Test_19th_June_2025;
+
+public class Coding_challenge_2
+    /*Arithmetic and Assignment Operators
+
+Create a Java program that demonstrates various arithmetic and assignment operators.
+
+**Requirements:**
+- Use +, -, *, /, % operators
+- Demonstrate assignment operators (=, +=, -=, *=, /=)
+- Show the results of each operation
+Addition: 15 Subtraction: 5 Multiplication: 50 Division: 2 Modulus: 0
+*/
+
+{
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 5;
+
+        int add = x+y;
+        int sub = x-y;
+        int multi = x*y;
+        int div = x/y;
+        int mod = x%2;
+        System.out.print("Addition: "+add+" Subtraction: "+sub+" Multiplication: "+multi+" Division: "+div+" Modulus: "+mod);
+    }
+}
