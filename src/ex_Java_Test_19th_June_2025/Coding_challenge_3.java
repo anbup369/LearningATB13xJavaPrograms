5bb15aa74c7b2480ed7ccfb5ff849aa6dc9e8eef
anbup369 <anbu.vpm@gmail.com>
2025-06-19 20:10:36 +0530

    Coding Challenge_3 : Comparison and Logical Operators

---
+++ src/ex_Java_Test_19th_June_2025/Coding_challenge_3.java
@@ -0,0 +1,17 @@
+package ex_Java_Test_19th_June_2025;
+
+public class Coding_challenge_3 {
+    /*Comparison and Logical Operators
+
+Write a program that demonstrates comparison and logical operators.
+Explanation:Program compares two numbers using various operators
+**Requirements:**
+- Use comparison operators (==, !=, <, >, <=, >=)
+- Use logical operators (&&, ||, !)
+- Print boolean results with explanatory messages
+input 10 5
+output
+a == b: false a > b: true a < b: false (a > b) && (a > 0): true (a < b) || (a > 0): true !(a > b): false
+*/
+
+}
