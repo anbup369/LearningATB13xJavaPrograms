e201968ea75fda425cbad34aabcae2a8e05870b3
anbup369 <anbu.vpm@gmail.com>
2025-06-19 19:57:44 +0530

    Coding Challenge_1 : Variable Declaration and Initialization

---
+++ src/ex_Java_Test_19th_June_2025/Coding_Challenge_1.java
@@ -0,0 +1,28 @@
+package ex_Java_Test_19th_June_2025;
+
+public class Coding_Challenge_1
+/*Variable Declaration and Initialization
+
+Write a Java program that demonstrates proper variable declaration and initialization for different data types.
+
+**Requirements:**
+- Declare and initialize variables of different primitive data types (int, double, boolean, char)
+- Print the values of all variables
+- Show proper naming convention
+
+Integer: 25 Double: 3.14 Boolean: true Character: A
+
+
+*/
+{
+    public static void main(String[] args) {
+        int Integer = 25;
+        double Double = 3.14;
+        boolean Boolean = true;
+        char Character = 'A';
+
+        System.out.print("Integer: "+Integer+" Double: "+Double+" Boolean: "+Boolean+" Character: "+Character);
+    }
+
+
+}
