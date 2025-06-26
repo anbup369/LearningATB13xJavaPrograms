e0bf6d4a3e35a565b433c840ee8540f5edbb76a0
anbu369 <anbu.vpm@gmail.com>
2025-06-26 18:40:07 +0530

    Java Strings:Coding_Challenge_1:String Manipulation Basics

---
+++ src/ex_Java_Test_26th_June_2025/Coding_Challenge_1.java
@@ -0,0 +1,28 @@
+package ex_Java_Test_26th_June_2025;
+
+public class Coding_Challenge_1 {
+    public static void main(String[] args) {
+        //String Manipulation Basics
+//        Write a Java program that demonstrates basic string operations including concatenation, length, substring, and character extraction.
+//        Examples:
+//        Input:
+//        Hello World
+//        Output:
+//        Length: 11, First char: H, Substring: World
+//💡 Explanation:Program performs basic string operations
+
+        //String declaration
+        String s = "Hello World";
+
+        //Getting Length of string
+        System.out.println("Length:"+s.length());
+
+        //Getting First char of string
+        System.out.println("First char:"+s.charAt(0));
+
+        //Getting substring World
+        System.out.println("substring:"+s.substring(6,11));
+
+
+    }
+}
